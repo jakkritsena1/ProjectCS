08375de74b746d228daef5de9be1cdd3ef967a0e
jakkritsena1 <112282385+jakkritsena1@users.noreply.github.com>
2024-04-10 20:27:12 +0700

    Add project files.

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            mapToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            label1 = new Label();
+            label2 = new Label();
+            table1 = new DataGridView();
+            table2 = new DataGridView();
+            button1 = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)table1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)table2).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, mapToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1029, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // mapToolStripMenuItem
+            // 
+            mapToolStripMenuItem.Name = "mapToolStripMenuItem";
+            mapToolStripMenuItem.Size = new Size(48, 20);
+            mapToolStripMenuItem.Text = "Maps";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(29, 56);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(87, 19);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Starting VEs";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(122, 56);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(70, 19);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "New VEs";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(215, 56);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(76, 19);
+            radioButton3.TabIndex = 3;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "%Change";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(308, 56);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(86, 19);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Corrections";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(401, 56);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(82, 19);
+            radioButton5.TabIndex = 5;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Hit Counts";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(189, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Front New VEs-TPS vs RPM";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(702, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(144, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Rear New VEs-TPS vs RPM";
+            // 
+            // table1
+            // 
+            table1.AllowUserToAddRows = false;
+            table1.AllowUserToDeleteRows = false;
+            table1.AllowUserToResizeColumns = false;
+            table1.AllowUserToResizeRows = false;
+            table1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            table1.Location = new Point(29, 122);
+            table1.Name = "table1";
+            table1.ReadOnly = true;
+            table1.RowHeadersWidth = 100;
+            table1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            table1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            table1.Size = new Size(454, 502);
+            table1.TabIndex = 6;
+            table1.CellContentClick += table1_CellContentClick;
+            // 
+            // table2
+            // 
+            table2.AllowUserToAddRows = false;
+            table2.AllowUserToDeleteRows = false;
+            table2.AllowUserToResizeColumns = false;
+            table2.AllowUserToResizeRows = false;
+            table2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            table2.Location = new Point(536, 122);
+            table2.Name = "table2";
+            table2.ReadOnly = true;
+            table2.RowHeadersWidth = 100;
+            table2.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            table2.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            table2.Size = new Size(454, 502);
+            table2.TabIndex = 10;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(29, 641);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 29);
+            button1.TabIndex = 11;
+            button1.Text = "Load Logs";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1029, 691);
+            Controls.Add(button1);
+            Controls.Add(table2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(table1);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)table1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)table2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem mapToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private Label label1;
+        private Label label2;
+        private DataGridView table1;
+        private DataGridView table2;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private String[] tps = { "0","2.3", "5.0", "10", "15", "20", "30", "40", "60", "80", "100" };
+        private String[] rpm = { "750", "1000", "1250", "1500", "1750", "2000", "2250", "2500", "2750", "3000", "3500", "4000", "4500", "5000", "5500", "6000", "6500", "7000" };
+        private double[,] dataSetVeFront = new double[18, 11];
+        private double[,] dataSetVeRear = new double[18, 11];
+        private string selectedFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string tp in tps)
+            {
+                table1.Columns.Add(tp, tp);
+                table2.Columns.Add(tp, tp);
+            }
+            table1.Rows.Add(18);
+            table2.Rows.Add(18);
+            foreach (DataGridViewColumn column in table1.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                column.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+            foreach (DataGridViewColumn column in table2.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                column.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+            int index = 0;
+            foreach (string rp in rpm)
+            {
+                table1.Rows[index].HeaderCell.Value = rp;
+                table2.Rows[index].HeaderCell.Value = rp;
+                index++;
+            }
+        }
+
+        private void table1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openfile = new OpenFileDialog())
+            {
+                openfile.InitialDirectory = "c:/";
+                openfile.Filter = "Excel Files (*.csv*)|*.csv*";
+                DialogResult result = openfile.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string selectedFile = openfile.FileName;
+                    radioButton2.Enabled = true;
+                    radioButton3.Enabled = true;
+                    radioButton4.Enabled = true;
+                    radioButton5.Enabled = true;
+                }
+                else
+                {
+                    radioButton2.Enabled = false;
+                    MessageBox.Show(this,"Invalid file.");
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    table1.Rows[i].Cells[j].Value = dataSetVeFront[i, j];
+                    table2.Rows[i].Cells[j].Value = dataSetVeRear[i, j];
+                }
+            }
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+            radioButton4.Enabled = false;
+            radioButton5.Enabled = false;
+        }
+
+        private void feedforward_netural_network(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (selectedFile != null && File.Exists(selectedFile)) 
+            {
+                string[] selectFileLines = File.ReadAllLines(selectedFile);
+                foreach (string line in selectFileLines)
+                {
+                    string[] fields = line.Split(',');
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid CSV file."); 
+            }
+        }
+
+    }
+}
